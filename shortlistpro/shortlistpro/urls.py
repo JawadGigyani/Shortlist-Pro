46ca857fc26b4024dc0780466572433a9d857d27
JawadGigyani <jawadkhangigyani@gmail.com>
2025-07-12 05:33:42 +0500

    Added Login Button

--- shortlistpro/shortlistpro/urls.py
+++ shortlistpro/shortlistpro/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
-    path('', include('home.auth_urls')),
+    # path('', include('home.auth_urls')),
+    # path('', include('home.register_urls')),
 ]
